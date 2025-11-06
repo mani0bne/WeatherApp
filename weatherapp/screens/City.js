fbb7058141a27a7cedba1897bd9a3465f7ebe1c6
Manihas Netha N <119604785+mani0bne@users.noreply.github.com>
2025-11-06 18:14:19 +0530

    Refactor City component with timezone support
    Updated the City component to use moment-timezone for sunrise and sunset times, and improved styling.

--- weatherapp/screens/City.js
+++ weatherapp/screens/City.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import {
   SafeAreaView,
   Text,
@@ -6,114 +6,132 @@ import {
   ImageBackground,
   StatusBar,
   View
-} from 'react-native'
-import moment from 'moment'
-import IconText from '../components/Icontext'
+} from 'react-native';
+import moment from 'moment-timezone';
+import IconText from '../components/Icontext';
 
 const City = ({ weatherData }) => {
-  const { name, country, population, sunrise, sunset } = weatherData
+  const { name, country, population, sunrise, sunset } = weatherData;
   const {
     container,
     cityName,
-    cityText,
     countryName,
+    cityText,
     imageLayout,
     populationWrapper,
     populationText,
     riseSetWrapper,
     riseSetText,
     rowLayout
-  } = styles
+  } = styles;
+
   return (
     <SafeAreaView style={container}>
       <ImageBackground
         source={require('../assets/city-background.jpg')}
         style={imageLayout}
+        resizeMode="cover"
       >
+        {/* CITY NAME */}
         <Text style={[cityName, cityText]}>{name}</Text>
         <Text style={[countryName, cityText]}>{country}</Text>
+
+        {/* POPULATION */}
         <View style={[populationWrapper, rowLayout]}>
           <IconText
             iconName={'user'}
-            iconColor={'red'}
+            iconColor={'#ff4444'}
             bodyText={`Population: ${population}`}
             bodyTextStyles={populationText}
           />
         </View>
+
+        {/* SUNRISE / SUNSET */}
         <View style={[riseSetWrapper, rowLayout]}>
           <IconText
             iconName={'sunrise'}
-            iconColor={'white'}
-            bodyText={moment(sunrise).format('h:mm:ss a')}
+            iconColor={'#FFDD55'}
+            bodyText={moment.unix(sunrise).tz("Asia/Kolkata").format("h:mm a")}
             bodyTextStyles={riseSetText}
           />
+
           <IconText
             iconName={'sunset'}
-            iconColor={'white'}
-            bodyText={moment(sunset).format('h:mm:ss a')}
+            iconColor={'#FFAA33'}
+            bodyText={moment.unix(sunset).tz("Asia/Kolkata").format("h:mm a")}
             bodyTextStyles={riseSetText}
           />
         </View>
+
       </ImageBackground>
     </SafeAreaView>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#000'
   },
 
   imageLayout: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
+    justifyContent: "center",
+    paddingHorizontal: 20
   },
 
   cityName: {
-    fontSize: 40,
-  },
-  countryName: {
-    fontSize: 30,
-  },
-
-  cityText : {
-    justifyContent: 'center',
-    alignSelf: 'center',
-    fontWeight: 'bold',
+    fontSize: 48,
+    textAlign: 'center',
+    fontWeight: '900',
     color: 'white',
+    textShadowColor: '#000',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 5
+  },
 
+  countryName: {
+    fontSize: 28,
+    textAlign: 'center',
+    fontWeight: '600',
+    color: '#eaeaea',
+    marginBottom: 10
+  },
+
+  cityText: {
+    alignSelf: 'center'
   },
 
   populationWrapper: {
-    justifyContent: 'center',
-    marginTop: 30
-
+    marginTop: 20,
+    justifyContent: 'center'
   },
 
-  populationText:{
-    color: 'red',
-    fontSize: 25,
-    marginLeft: 7.5
+  populationText: {
+    color: '#ff4444',
+    fontSize: 22,
+    marginLeft: 10,
+    fontWeight: '600'
   },
 
-  risesetWrapper : {
-    marginTop: 30,
+  riseSetWrapper: {
+    marginTop: 40,
     justifyContent: 'space-between',
-    flexDirection: 'row'
-    
+    width: "100%"
+  },
 
-  },
- rowLayout : {
-    flexDirection : 'row',
-    alignItems: 'center'
-  },
   riseSetText: {
-    fontSize: 20,
-    color: 'white'
+    fontSize: 22,
+    color: 'white',
+    fontWeight: '500',
+    marginLeft: 8
+  },
+
+  rowLayout: {
+    flexDirection: 'row',
+    alignItems: 'center'
   }
+});
 
-
-
-})
 export default City;
